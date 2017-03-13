e5760efbfee78ed9740790fc637d2d6097bbefe0
David Norton <dnorton@object-partners.local>
2017-03-13 12:33:32 -0500

    try to build pipelines from job DSL

---
+++ pipelines.groovy
@@ -0,0 +1,30 @@
+['deployment-pipeline'].each { String projectName ->
+  pipelineJob(projectName) {
+    definition {
+      cps {
+        node {
+            stage('Dev') {
+                milestone 1
+                sh "echo DEV"
+                input message: 'Promote  to QA?', ok: 'Promote'
+            }
+            stage('QA') {
+                milestone 2
+                sh "echo QA"
+                input message: 'Promote  to Staging?', ok: 'Promote'
+            }
+            stage('Staging') {
+                milestone 3
+                sh "echo Staging"
+                input message: 'Promote  to Production?', ok: 'Promote'
+            }
+            stage('Production') {
+                milestone 4
+                concurrency = 1
+                sh "echo Production"
+            }
+        }
+      }
+    }
+  }
+}
